73669c03eed51dad8e653808904894b16ea2f11e
Emmanuel Chassot <EChassot@iotc.local>
2025-02-11 18:08:08 +0400

    Updates in SQL queries

--- initialization/01_GTA_NC_DATA_EXTRACTION.R
+++ initialization/01_GTA_NC_DATA_EXTRACTION.R
@@ -1,4 +1,3 @@
-
 print("Initializing data extraction...")
 
 # DESCRIPTION OF CONTENTS #### 
@@ -11,7 +10,6 @@ NC_YEAR_COVERAGE_RFMO       = dbGetQuery(con_GTA, QUERY_NC_YEAR_COVERAGE_RFMO)
 
 ## FISHING GEARS ####
 
-
 ## SPECIES ####
 
 # Full list of species for all RFMOs with catch
@@ -22,28 +20,33 @@ NC_SPECIES_LIST_GLOBAL = as.data.table(dbGetQuery(con_GTA, QUERY_NC_SPECIES_LIST
 # Total number of species by species group
 # No filter on catch
 QUERY_GTA_LIST_NC_SPECIES_GLOBAL_SPECIES_GROUP_TABLE = read_file("../inputs/queries/GTA_LIST_NC_SPECIES_ALL_SPECIES_GROUP_TABLE.sql")
+
 NC_SPECIES_NUMBERS_BY_SPECIES_GROUP_GLOBAL_TABLE = as.data.table(dbGetQuery(con_GTA, QUERY_GTA_LIST_NC_SPECIES_GLOBAL_SPECIES_GROUP_TABLE))
 
 # Total number of species by family
 # No filter on catch
 QUERY_GTA_LIST_NC_SPECIES_GLOBAL_FAMILY_TABLE = read_file("../inputs/queries/GTA_LIST_NC_SPECIES_ALL_FAMILY_TABLE.sql")
+
 NC_SPECIES_NUMBERS_BY_FAMILY_GLOBAL_TABLE = as.data.table(dbGetQuery(con_GTA, QUERY_GTA_LIST_NC_SPECIES_GLOBAL_FAMILY_TABLE))
 
 # Full list of species by RFMO
 # No filter
 QUERY_NC_SPECIES_LIST_RFMO = read_file("../inputs/queries/GTA_LIST_NC_SPECIES_BY_RFMO.sql")
+
 NC_SPECIES_LIST_RFMO = as.data.table(dbGetQuery(con_GTA, QUERY_NC_SPECIES_LIST_RFMO))
 
 # Table by species group
 # Trick in the query to removes the errors of reporting of species with a catch threshold
 # CAUTION: the threshold will remove rare species, including slender tuna and Mediterannean spearfish from the data set
 QUERY_GTA_LIST_NC_SPECIES_BY_RFMO_SPECIES_GROUP_TABLE = read_file("../inputs/queries/GTA_LIST_NC_SPECIES_BY_RFMO_SPECIES_GROUP_TABLE.sql")
+
 NC_SPECIES_NUMBERS_BY_SPECIES_GROUP_RFMO_TABLE = as.data.table(dbGetQuery(con_GTA, QUERY_GTA_LIST_NC_SPECIES_BY_RFMO_SPECIES_GROUP_TABLE))
 
 # Table by family
 # Trick in the query to removes the errors of reporting of species with a catch threshold
 # CAUTION: the threshold will remove rare species, including slender tuna and Mediterannean spearfish from the data set
 QUERY_GTA_LIST_NC_SPECIES_BY_RFMO_FAMILY_TABLE = read_file("../inputs/queries/GTA_LIST_NC_SPECIES_BY_RFMO_FAMILY_TABLE.sql")
+
 NC_SPECIES_NUMBERS_BY_FAMILY_RFMO_TABLE = as.data.table(dbGetQuery(con_GTA, QUERY_GTA_LIST_NC_SPECIES_BY_RFMO_FAMILY_TABLE))
 
 # ALL SPECIES ####
--- inputs/queries/GTA_LIST_NC_SPECIES_ALL_SPECIES_GROUP_TABLE.sql
+++ inputs/queries/GTA_LIST_NC_SPECIES_ALL_SPECIES_GROUP_TABLE.sql
@@ -2,14 +2,14 @@ WITH SPECIES_ALL AS
 (
   SELECT DISTINCT nc.species_group AS "SPECIES_GROUP_CODE",
   sg.label AS "SPECIES_GROUP", sp.taxa_order AS "ORDER", sp.taxa_family AS "FAMILY",
-  nc.species AS "SPECIES_CODE", sp.label AS "SPECIES", sp.taxa_scientific_name AS "SPECIES_SCIENTIFIC", SUM(value) AS CATCH
+  nc.species AS "SPECIES_CODE", sp.label AS "SPECIES", sp.taxa_scientific_name AS "SPECIES_SCIENTIFIC", SUM(measurement_value) AS CATCH
   FROM fact_tables.global_nominal_catch_firms_level0 nc
   LEFT JOIN species.species_asfis sp ON (nc.species = sp.code)
   LEFT JOIN species.speciesgroup_tunaatlas sg ON (nc.species_group = sg.code)
   WHERE sp.label NOT LIKE '%nei'
   AND nc.species NOT LIKE 'FRZ'
   GROUP BY nc.species_group, sg.label, sp.taxa_order, sp.taxa_family, nc.species, sp.label, sp.taxa_scientific_name
-  ORDER BY SUM(value) DESC
+  ORDER BY SUM(measurement_value) DESC
 )
 
 SELECT "SPECIES_GROUP", COUNT(*) AS N
